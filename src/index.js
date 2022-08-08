d14cd47f51c65f7fe58c6bd3839f3fed0ff2cdb1
Natalia-Agudelo <108898634+Natalia-Agudelo@users.noreply.github.com>
2022-08-08 12:43:05 -0500

    actialización proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+const btnVerificar = document.getElementById("verificar");
+btnVerificar.addEventListener("click", numeroDeTj);
+
+function numeroDeTj() {
+  let inputvalue = document.getElementById("numeroDeTj").value;
+  // llamar la funcion
+  console.log(validator.isValid(inputvalue));
+  //
+  console.log(validator.maskify(inputvalue))
+}
+//let creditCardNumber = document.getElementById("numeroDeTj").value
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,82 @@
 const validator = {
-  // ...
+  isValid: function (creditCardNumber) {
+    let numeroTarjetaRever = "";
+
+    // para revesar el numero //
+
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+      numeroTarjetaRever += creditCardNumber[i];
+      console.log(creditCardNumber[i]);
+    }
+   
+
+    // convertir string en array
+
+    let numeroArray = numeroTarjetaRever.split("");
+    console.log(numeroArray);
+
+    // seleccion de numeros en posicion par y multiplicacion de cada numero *2
+
+    let nDobles = [];
+    numeroArray.forEach((element, index) => {
+      if (index % 2 > 0) {
+        nDobles.push(element * 2);
+        console.log(nDobles);
+      } else {
+        nDobles.push(parseInt(element));
+      }
+     
+    });
+
+    // SELECCIONAR NUMEROS MAYORES A 9
+    let nMayores = [];
+    nDobles.forEach((element) => {
+      if (element > 9) {
+        nMayores.push(element - 9);
+        console.log(nMayores);
+      } else {
+        nMayores.push(element);
+        
+      }
+    });
+
+    //SUMA DE NUMEROS EN TODAS LAS POSICIONES
+
+    let nSumado = 0;
+
+    for (let i = 0; i < nMayores.length; i++) {
+      nSumado += nMayores[i];
+    }
+    //RETORNAR SI ES VALIDA O NO
+    if (nSumado % 10 === 0) {
+      return true;
+      // alert("Valida");
+    } else {
+      return false;
+      // alert("Invalida");
+    }
+    
+  },
+  // MASKIFY
+  maskify: function (creditCardNumber) {
+    let numeroMaski = creditCardNumber.split("");
+
+    let numeros12Masky = numeroMaski.slice(0, -4).join('').replace(/\w/g, "#");
+   
+
+    let numerosSinM = numeroMaski.slice(-4);
+    
+
+    let union =numeros12Masky+numerosSinM.join('');
+   
+
+    return union
+  },
+   
+
 };
+
+//validator.maskify('4556364607935616'); // ############5616
+// 1 // null // ()
 
 export default validator;
